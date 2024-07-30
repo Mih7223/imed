79e148e3a40cbe082594ad89747382ce6fa1d3a3
unknown <mihoubiimed@yahoo.fr>
2024-07-30 21:24:14 +0200

    Initial commit

---
+++ articles/urls.py
@@ -0,0 +1,18 @@
+
+from django.contrib import admin
+from django.urls import path, include
+
+from . import views
+
+
+
+app_name = 'articles'
+
+
+urlpatterns = [
+
+    path('articles/', views.articles_view, name='articles'),
+    path('creer/', views.creer_view,name='creer'),
+    path('<slug:slug>/', views.article_view, name='article'),
+
+]
